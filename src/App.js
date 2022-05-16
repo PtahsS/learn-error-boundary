7bcfa9d8dde67b04c83d389f0b4a067416fa7c92
Serhii <ptahsrg@gmail.com>
2022-05-16 23:45:52 +0300

    c1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+import { withErrorBoundary, ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback';
+
+function Bomb({ username }) {
+  if (username === 'bomb') {
+    throw new Error("CABOOM");
+  }
+  return `Hi ${username}`
+}
+
 function App() {
+  const [username, setUsername] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <header className='App-header'>
+        <label>
+          {`Username: `}
+          <input
+            placeholder={`type "bomb"`}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <ErrorBoundary 
+        FallbackComponent={ErrorFallback}
+        // onReset={() => setUsername("")}
+        resetKeys={[username]}
         >
-          Learn React
-        </a>
+          <Bomb username={username} />
+        </ErrorBoundary>
       </header>
+
     </div>
   );
 }
+
+// export default withErrorBoundary(App, {
+//   fallback: <div>Что-то пошло не так</div>,
+//   fallbackRender: () => <div>Что-то пошло не так</div>,
+//   // FallbackComponent,
+// });
 
 export default App;
